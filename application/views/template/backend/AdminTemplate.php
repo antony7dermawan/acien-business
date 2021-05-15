8095175cff237f30cb2398d77f41e9458c8cba2f
antony7dermawan <62688094+antony7dermawan@users.noreply.github.com>
2021-05-15 15:39:25 +0700

    up

--- application/views/template/backend/AdminTemplate.php
+++ application/views/template/backend/AdminTemplate.php
@@ -6,7 +6,7 @@
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
 
 <head>
-    <title>CV JAYA ABADI</title>
+    <title>PT. CAHAYA BARU GEMILANG</title>
 
 
     <!--[if lt IE 10]>
